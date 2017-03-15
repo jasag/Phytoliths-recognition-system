71301a7aba876207ef8903344038dd40322df742
Jaime Sagüillo <jaime.saguillo@gmail.com>
2017-03-15 16:49:30 +0100

    #19 Añadidos clasificador bayesiano y clasificador mediante regresión logística

--- code/src/classifiers.py
+++ code/src/classifiers.py
@@ -16,10 +16,13 @@ svm_path = path + 'svm_clf.sav'
 svm2_path = path + 'svm2_clf.sav'
 gtb_path = path + 'gtb_clf.sav'
 rf_path = path + 'rf_clf.sav'
-
+regr_path = path + 'rgr_clf.sav'
+gnb_path = path + 'gnb_clf.sav'
 
 # Serizalización: Cargamos clasificador e indicamos si devuelve probabilidades
 classifiers['svm'] = [pickle.load(open(svm_path, 'rb')), False]
 classifiers['svm2'] = [pickle.load(open(svm2_path, 'rb')), True]
 classifiers['gtb'] = [pickle.load(open(gtb_path, 'rb')), True]
 classifiers['rf'] = [pickle.load(open(rf_path, 'rb')), True]
+classifiers['regr'] = [pickle.load(open(regr_path, 'rb')), False]
+classifiers['gnb'] = [pickle.load(open(gnb_path, 'rb')), True]
